a34c692d5040d55018ccd530b7b69f64bd91d6eb
simil.mittal <simil.mittal@algosec.com>
2022-11-04 13:11:32 +0530

    SG with SG rule

--- src/AWS/Scenario 1/main.tf
+++ src/AWS/Scenario 1/main.tf
@@ -1,23 +1,27 @@
 provider "aws" {
-  region     = "us-west-1"
+  region = "us-west-1"
 }
 
 resource "aws_security_group" "Scenario1" {
-  name        = "SG with all port and all IP. "
-  description = "Ingress"
+
+  name        = "SG with all port and all IP."
   vpc_id      = "vpc-481b202f"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
+  description = "Ingress"
+  tags        = {
     Name = "Scenario1"
   }
 }
+
+resource "aws_security_group_rule" "ingress" {
+
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.Scenario1.id
+}
+
 
 
 
