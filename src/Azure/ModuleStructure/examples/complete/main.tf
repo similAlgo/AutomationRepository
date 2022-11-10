971e505ebabddda7605344d2f4ef3c88c47cdbc8
simil.mittal <simil.mittal@algosec.com>
2022-11-10 13:02:17 +0530

    Module structure upload

--- src/Azure/ModuleStructure/examples/complete/main.tf
+++ src/Azure/ModuleStructure/examples/complete/main.tf
@@ -1,27 +1,31 @@
-module "vnet-hub" {
-  source  = "kumarvna/caf-virtual-network-hub/azurerm"
-  version = "2.0.0"
+provider "azurerm" {
+  features {}
+}
+
+module "vnet_hub" {
+  source  = "../../"
+  # version = "2.0.0"
 
   # By default, this module will create a resource group, proivde the name here
   # to use an existing resource group, specify the existing resource group name, 
   # and set the argument to `create_resource_group = false`. Location will be same as existing RG. 
-  resource_group_name = "rg-hub-demo-internal-shared-westeurope-001"
-  location            = "westeurope"
-  hub_vnet_name       = "default-hub"
+  # resource_group_name = "rg-hub-demo-internal-shared-westeurope-001"
+  # location            = "westeurope"
+  # hub_vnet_name       = "default-hub"
 
   # Provide valid VNet Address space and specify valid domain name for Private DNS Zone.  
-  vnet_address_space             = ["10.1.0.0/16"]
-  firewall_subnet_address_prefix = ["10.1.0.0/26"]
-  gateway_subnet_address_prefix  = ["10.1.1.0/27"]
-  private_dns_zone_name          = "publiccloud.example.com"
+  # vnet_address_space             = ["10.1.0.0/16"]
+  # firewall_subnet_address_prefix = ["10.1.0.0/26"]
+  # gateway_subnet_address_prefix  = ["10.1.1.0/27"]
+  # private_dns_zone_name          = "publiccloud.example.com"
 
   # (Required) To enable Azure Monitoring and flow logs
   # Log Retention in days - Possible values range between 30 and 730
-  log_analytics_workspace_sku          = "PerGB2018"
-  log_analytics_logs_retention_in_days = 30
+  # log_analytics_workspace_sku          = "PerGB2018"
+  # log_analytics_logs_retention_in_days = 30
 
   # Adding Standard DDoS Plan, and custom DNS servers (Optional)
-  dns_servers = []
+  # dns_servers = []
 
   # Multiple Subnets, Service delegation, Service Endpoints, Network security groups
   # These are default subnets with required configuration, check README.md for more details
@@ -29,6 +33,8 @@ module "vnet-hub" {
   # First two address ranges from VNet Address space reserved for Gateway And Firewall Subnets. 
   # ex.: For 10.1.0.0/16 address space, usable address range start from 10.1.2.0/24 for all subnets.
   # subnet name will be set as per Azure naming convention by defaut. expected value here is: <App or project name>
+  location="East US"
+  resource_group_name = "devsecops-resources"
   subnets = {
     mgnt_subnet = {
       subnet_name           = "management"
@@ -70,60 +76,60 @@ module "vnet-hub" {
     }
   }
 
-  # (Optional) To enable the availability zones for firewall. 
-  # Availability Zones can only be configured during deployment 
-  # You can't modify an existing firewall to include Availability Zones
-  firewall_zones = [1, 2, 3]
+  # # (Optional) To enable the availability zones for firewall. 
+  # # Availability Zones can only be configured during deployment 
+  # # You can't modify an existing firewall to include Availability Zones
+  # firewall_zones = [1, 2, 3]
 
-  # (Optional) specify the application rules for Azure Firewall
-  firewall_application_rules = [
-    {
-      name             = "microsoft"
-      action           = "Allow"
-      source_addresses = ["10.0.0.0/8"]
-      target_fqdns     = ["*.microsoft.com"]
-      protocol = {
-        type = "Http"
-        port = "80"
-      }
-    },
-  ]
+  # # (Optional) specify the application rules for Azure Firewall
+  # firewall_application_rules = [
+  #   {
+  #     name             = "microsoft"
+  #     action           = "Allow"
+  #     source_addresses = ["10.0.0.0/8"]
+  #     target_fqdns     = ["*.microsoft.com"]
+  #     protocol = {
+  #       type = "Http"
+  #       port = "80"
+  #     }
+  #   },
+  # ]
 
-  # (Optional) specify the Network rules for Azure Firewall
-  firewall_network_rules = [
-    {
-      name                  = "ntp"
-      action                = "Allow"
-      source_addresses      = ["10.0.0.0/8"]
-      destination_ports     = ["123"]
-      destination_addresses = ["*"]
-      protocols             = ["UDP"]
-    },
-  ]
+  # # (Optional) specify the Network rules for Azure Firewall
+  # firewall_network_rules = [
+  #   {
+  #     name                  = "ntp"
+  #     action                = "Allow"
+  #     source_addresses      = ["10.0.0.0/8"]
+  #     destination_ports     = ["123"]
+  #     destination_addresses = ["*"]
+  #     protocols             = ["UDP"]
+  #   },
+  # ]
 
-  # (Optional) specify the NAT rules for Azure Firewall
-  # Destination address must be Firewall public IP
-  # `fw-public` is a variable value and automatically pick the firewall public IP from module.
-  firewall_nat_rules = [
-    {
-      name                  = "testrule"
-      action                = "Dnat"
-      source_addresses      = ["10.0.0.0/8"]
-      destination_ports     = ["53", ]
-      destination_addresses = ["fw-public"]
-      translated_port       = 53
-      translated_address    = "8.8.8.8"
-      protocols             = ["TCP", "UDP", ]
-    },
-  ]
+  # # (Optional) specify the NAT rules for Azure Firewall
+  # # Destination address must be Firewall public IP
+  # # `fw-public` is a variable value and automatically pick the firewall public IP from module.
+  # firewall_nat_rules = [
+  #   {
+  #     name                  = "testrule"
+  #     action                = "Dnat"
+  #     source_addresses      = ["10.0.0.0/8"]
+  #     destination_ports     = ["53", ]
+  #     destination_addresses = ["fw-public"]
+  #     translated_port       = 53
+  #     translated_address    = "8.8.8.8"
+  #     protocols             = ["TCP", "UDP", ]
+  #   },
+  # ]
 
-  # Adding TAG's to your Azure resources (Required)
-  # ProjectName and Env are already declared above, to use them here, create a varible. 
-  tags = {
-    ProjectName  = "demo-internal"
-    Env          = "dev"
-    Owner        = "user@example.com"
-    BusinessUnit = "CORP"
-    ServiceClass = "Gold"
-  }
+  # # Adding TAG's to your Azure resources (Required)
+  # # ProjectName and Env are already declared above, to use them here, create a varible. 
+  # tags = {
+  #   ProjectName  = "demo-internal"
+  #   Env          = "dev"
+  #   Owner        = "user@example.com"
+  #   BusinessUnit = "CORP"
+  #   ServiceClass = "Gold"
+  # }
 }
--- src/Azure/ModuleStructure/examples/complete/versions.tf
+++ src/Azure/ModuleStructure/examples/complete/versions.tf
@@ -11,6 +11,3 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "azurerm" {
-  features {}
-}
