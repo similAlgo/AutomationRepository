e6d2e3843fd8ad4388e97508d49833dceb2e40c2
DevSecOpsAutomation <115538844+DevSecOpsAutomation@users.noreply.github.com>
2022-11-18 16:54:19 +0530

    Update main.tf

--- src/AWS/Scenario 2/main.tf
+++ src/AWS/Scenario 2/main.tf
@@ -6,25 +6,27 @@ resource "aws_security_group" "Scenario2" {
   name        = "SG with Single port(UDP and TCP) and all IP."
   description = "Ingress"
   vpc_id      = "vpc-481b202f"
-
-  ingress {
-    from_port   = 389
-    to_port     = 389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 389
-    to_port     = 389
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "Scenario2"
   }
 }
 
+resource "aws_security_group_rule" "ingressrule1" {
+  type              = "ingress"
+  from_port         = 389
+  to_port           = 389
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.Scenario2.id
+}
+
+resource "aws_security_group_rule" "ingressrule1" {
+  type              = "ingress"
+  from_port         = 389
+  to_port           = 389
+  protocol          = "udp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.Scenario2.id
+}
 
 
